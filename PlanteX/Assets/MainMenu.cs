d613fdd05d80e5f1eebc61aafaf906b8ef49fa43
kaarinlt <43746516+kaarinlt@users.noreply.github.com>
2019-05-15 13:35:07 +0200

    main menu

---
+++ PlanteX/Assets/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button newGameButton;
+
+    public Button loadButton;
+
+    public Button exitGameButton;
+
+    public string newGameSceneName;
+
+    public GameObject loadGameMenu;
+
+    public void Awake(){
+        newGameButton.onClick.AddListener(NewGame);
+        loadButton.onClick.AddListener(OpenLoadGameMenu);
+        exitGameButton.onClick.AddListener(ExitGame);
+    }
+
+    public void NewGame(){
+        SceneManager.LoadScene(newGameSceneName);
+    }
+    public void OpenLoadGameMenu(){
+        loadGameMenu.SetActive(true);
+    }
+
+    public void ExitGame(){
+        Debug.Log("trying to exit game. Doesn't work in editor.");
+        Application.Quit();
+    }
+}
