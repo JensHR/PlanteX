c5d6ff9050a902b3893273297a871b0b03199e49
drtpro <47092330+drtpro@users.noreply.github.com>
2019-05-20 10:58:25 +0200

    Endinger drt Ny pc til gammel
    Endinger drt Ny pc til gammel

---
+++ PlanteX/Assets/Scripts/Trigger/moveScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveScene : MonoBehaviour
+{
+    [SerializeField] private int loadLevel;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            int loadLevel = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+
+}
