5664545a80c78bd6feeb6cb99e0db5d792daa9e2
kaarinlt <43746516+kaarinlt@users.noreply.github.com>
2019-05-21 22:27:27 +0200

    Splash

---
+++ PlanteX/Assets/Scripts/LevelRules/SplashScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class SplashScreenManager : MonoBehaviour
+{
+    public MovieTexture SplashScreenTexture;
+    private AudioSource audio;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = SplashScreenTexture as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = SplashScreenTexture.audioClip;
+
+        SplashScreenTexture.Play();
+        audio.Play();
+    }
+
+    void Update()
+    {
+        if (!SplashScreenTexture.isPlaying)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
