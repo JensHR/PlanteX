ab826c19fb59b31d217e57a9669acd44d79ec0a6
kaarinlt <43746516+kaarinlt@users.noreply.github.com>
2019-05-21 19:41:10 +0200

    Steiner med mer

---
+++ PlanteX/Assets/Scripts/LevelRules/1002/LastBossLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastBossLogic : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject LastBoss;
+
+    void Update()
+    {
+        if(LastBoss == null)
+        {
+
+        }
+    }
+}
